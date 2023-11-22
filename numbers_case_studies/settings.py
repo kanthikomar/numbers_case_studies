11e2933689cd7cb8fcc0fdab82b8137765365ad5
Kanthi Komar <42362615+kanthikomar@users.noreply.github.com>
2023-11-21 21:14:34 -0800

    Update settings.py

--- numbers_case_studies/settings.py
+++ numbers_case_studies/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kxtc+w*2v=)!my#g-1ndp#_^8=iae%dkf1bl0fhwg&^odyi%_u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
